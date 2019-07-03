c10b70e72cac8bee5a48bcc41fbb632c6cdbf125
junelotus <572733610@qq.com>
2019-07-03 23:30:54 +0800

    Add files via upload

--- back_projecy.cpp
+++ back_projecy.cpp
@@ -28,20 +28,37 @@ calcHist(&src,1,chan_count,Mat(),h_hist,3,histSize,ranges,true,false);
 */
 //normalize(hist, hist, 0, 255, NORM_MINMAX, -1, Mat());
 cvtColor(src,hsv,CV_BGR2HSV);
- int histSize[]={100};
-const float range[]={0,180};
+ int histSize[]={25,25};
+const float range[]={0,256};
 const float *ranges[]={range,range,range};
 const int channels[] ={0,1,2};
 
 
-MatND hist;
-calcHist(&hsv,1,channels,Mat(),hist,1,histSize,ranges,true,false);
+MatND   hist;
+calcHist(&hsv,1,channels,Mat(),hist,2,histSize,ranges,true,false);
 normalize(hist, hist, 0, 255, NORM_MINMAX, -1, Mat());
 MatND dest;//(src.size(),CV_32FC1);
 calcBackProject(&hsv,1,channels,hist,dest,ranges);
 
+Mat dest_june(hsv.size(),CV_8UC1);
+
+for(int i=0;i<hsv.rows;i++)
+for(int j=0;j<hsv.cols;j++)
+{
+    float h = hsv.at<Vec3b>(i,j)[0];
+    float s = hsv.at<Vec3b>(i,j)[1];
+    float value = hist.at<float>(h/25,s/25);
+//cout<<value<<endl;
+    dest_june.at<unsigned char>(i,j) = static_cast<unsigned char>(value*255);
+
+}
+
+ 
+
+
 imshow("src",src);
 imshow("dest",dest);
+imshow("dest_june",dest_june);
 waitKey(100000);
 return 0;
 

04ab1dfa496625fee9ec117528136757804fcd4b
hjxiao <hjxiao@nullmax.ai>
2019-07-15 09:53:24 +0800

    test_32f_32fu1

---
+++ test_32f_32fU1.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+int main()
+{
+    std::cout <<  CV_32F << std::endl;
+    std::cout <<  CV_32FC1 << std::endl;
+
+    return 0;
+}
+
